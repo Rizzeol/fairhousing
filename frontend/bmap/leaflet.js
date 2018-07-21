94622b7c965362e23fb6889b7a6814b27ea7aee3
Rohit Paudel <rohit@NETCUS4-156.f246.berlin.impacthub.net>
2018-07-21 22:00:49 +0200

    Added map to the website using leaflet map

---
+++ frontend/bmap/leaflet.js
@@ -0,0 +1,13 @@
+var map = L.map( 'map', {
+    center: [52.5200, 13.4050],
+    minZoom: 9,
+    zoom: 2
+});
+
+L.tileLayer( 'http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+    subdomains: ['a','b','c']
+}).addTo( map );
+
+var feURL = jQuery( 'script[src$="leaflet.js"]' ).attr( 'src' ).replace( 'leaflet.js', '' )
+
